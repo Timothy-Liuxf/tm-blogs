35aee97997d4278d2abdc5194a812e69ceae2792
Timothy Liu <liuxf19@163.com>
2024-06-05 21:31:57 +0800

    fix(js): :bug: predefined config params are only available in _layouts

--- assets/js/prepare.js
+++ assets/js/prepare.js
@@ -1,7 +1,3 @@
-const base_url = "{{ site.github.url }}";
-const repo_url = "{{ site.github.repository_url }}";
-const repo_branch = "{{ site.github.source.branch }}";
-const repo_path = "{{ site.github.source.path }}";
 const full_url = window.location.href;
 const page_hash = window.location.hash;
 const page_search = window.location.search;
@@ -16,33 +12,3 @@ const params = current_url.split('?')[1];
 const hash = current_url.split('#')[1];
 
 document.documentElement.lang = lang_index !== -1 ? paths[lang_index] : 'en-US';
-
-const returnToBackPage = () => {
-    if (lang_index !== -1) {
-        if (lang_index + 2 === paths.length) {
-            window.location.href = paths.slice(0, lang_index).join('/') + '/';
-        } else {
-            window.location.href = paths.slice(0, lang_index + 1).join('/') + '/';
-        }
-    } else {
-        window.location.href = paths.slice(0, 4).join('/') + '/'
-    }
-};
-
-const viewOnGitHub = () => {
-    if (is_main_page) {
-        window.location.href = repo_url + '/';
-    } else {
-        let target = `${repo_url}/blob/${repo_branch}/${repo_path.replace(/\/$/, '')}`;
-        if (!target.endsWith('/')) {
-            target += '/';
-        }
-        target += current_url.slice(base_url.length + 1);
-        if (target.endsWith('/')) {
-            target += 'README.md';
-        } else if (target.endsWith('.html')) {
-            target = target.replace(/\.html$/, '.md');
-        }
-        window.location.href = target;
-    }
-};
